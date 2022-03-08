febf4626eb6dcecf5cde127a54fd42944da19390
agocskcs <ismeretlen1024@gmail.com>
2022-03-08 11:36:43 +0100

    Added timer

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from "react"
 import ReactDOM from "react-dom"
+import moment from "moment"
 
 function App() {
+    const [time, setTime] = useState("")
+
+    useEffect(() => {
+        setInterval(() => {
+            setTime(moment(1646735762823).fromNow())
+        }, 1000)
+    }, [])
+
     return <>
         <h1>Hello world!</h1>
         <p>Nothing is working as intended yet!</p>
+        <p>{ time }</p>
     </>
 }
 
