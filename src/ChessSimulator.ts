4fbb64c6d219ad606f6f15d9221075ca09921cf0
agocskcs <ismeretlen1024@gmail.com>
2022-03-08 11:33:46 +0100

    Simulator WIP part 0

--- src/ChessSimulator.ts
+++ src/ChessSimulator.ts
@@ -1,14 +1,46 @@
 namespace ChessSimulator {
+    const [X, Y] = [8, 8]
+
+    const _1DTo2D = (i: number) => [i % X, Math.floor(i / X)]
+
     enum BlackPieces {
-        
+        KING = "♚",
+        QUEEN = "♛",
+        ROOK = "♜",
+        BISHOP = "♝",
+        KNIGHT = "♞",
+        PAWN = "♟︎"
+    }
+    enum WhitePieces {
+        KING = "♔",
+        QUEEN = "♕",
+        ROOK = "♖",
+        BISHOP = "♗",
+        KNIGHT = "♘",
+        PAWN = "♙"
+    }
+    class PieceWrapper {
+        constructor(public readonly piece: BlackPieces | WhitePieces, public x = 0, public y = 0) {}
     }
 
     class Board {
+        public readonly tiles: PieceWrapper[]
 
+        constructor(tiles: (BlackPieces | WhitePieces)[]) {
+            this.tiles = Array<PieceWrapper>(X*Y).fill(undefined)
+
+            tiles.forEach((v, i) => {
+                this.tiles[i] = new PieceWrapper(v, ..._1DTo2D(i))
+            })
+        }
     }
 
     export class Simulator {
-        constructor() {}
+        constructor(private b: Board) {}
+
+        get board() {
+            return this.b
+        }
     }
 }
 
